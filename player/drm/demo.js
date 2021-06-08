e484d15707965c88c2d3e84bd82421f03d937165
Carlos Salazar <54643907+casalazarh@users.noreply.github.com>
2021-06-08 12:36:11 -0500

    Update demo.js

--- player/drm/demo.js
+++ player/drm/demo.js
@@ -1,5 +1,5 @@
 var config = {
-  key: '<YOUR PLAYER KEY>',
+  key: 'F73605D25B6E98673550D890258250B6',
   cast: {
     enable: true
   }
@@ -7,14 +7,14 @@ var config = {
 
 var source = {
   dash: 'https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/mpds/11331.mpd',
-  hls: 'https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/m3u8s/11331.m3u8',
+  hls: 'http://d1rlbhvkfp3998.cloudfront.net/out/v1/f12e90a266454d2d9cb83ece0710ce22/speke_el_hls.m3u8',
   smooth: 'https://test.playready.microsoft.com/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/manifest',
   drm: {
     widevine: {
-      LA_URL: 'https://widevine-proxy.appspot.com/proxy'
+      LA_URL: 'http://authorization.nowonline.com.br:8080/key=30111&prm=eyJjb250ZW50SWQiOiIzMDExMSIsImtleUlkIjoiMDE0N2ZkYzUtMjI2Zi00YWJmLWFmNjgtOWZiZjI4OGU0M2ZiIn0='
     },
     playready: {
-      LA_URL: 'https://playready.directtaps.net/pr/svc/rightsmanager.asmx?PlayRight=1&ContentKey=EAtsIJQPd5pFiRUrV9Layw=='
+      LA_URL: 'http://authorization.nowonline.com.br:8080/key=30111&prm=eyJjb250ZW50SWQiOiIzMDExMSIsImtleUlkIjoiMDE0N2ZkYzUtMjI2Zi00YWJmLWFmNjgtOWZiZjI4OGU0M2ZiIn0='
     }
   }
 };
